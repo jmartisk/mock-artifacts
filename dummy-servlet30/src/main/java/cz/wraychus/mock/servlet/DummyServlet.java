12851aced9d96b47c4edd90d8c5deab80d410bcf
Jan Martiska <jmartisk@redhat.com>
2012-12-06 11:18:20 +0100

    servlet apps

---
+++ dummy-servlet30/src/main/java/cz/wraychus/mock/servlet/DummyServlet.java
@@ -0,0 +1,20 @@
+package cz.wraychus.mock.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/")
+public class DummyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Hello world!");
+        out.flush();
+    }
+}
