d722e6a7875a0e83c54d6057182b81c2516ffca5
Jan Martiska <jmartisk@redhat.com>
2013-01-09 15:19:12 +0100

    more verbose scripts

--- jaxws/scripts/WSAddressing-FaultTo.sh
+++ jaxws/scripts/WSAddressing-FaultTo.sh
@@ -1,3 +1,7 @@
 # The service will throw a FAULT.
 # Thanks to WS-Addressing, you should receive the response (containing the fault) on port localhost:7777
+echo "************* Sending SOAP message: *************"
+cat msgs/SimpleWebService/throwFault
+echo "************* Response: *************"
 curl -d@msgs/SimpleWebService/throwFault http://localhost:8080/mock-jaxws/SimpleWebService
+echo ""
--- jaxws/scripts/WSAddressing-ReplyTo.sh
+++ jaxws/scripts/WSAddressing-ReplyTo.sh
@@ -1,2 +1,6 @@
 # Thanks to WS-Addressing, you should receive the response on port localhost:7778
+echo "************* Sending SOAP message: *************"
+cat msgs/SimpleWebService/throwFault
+echo "************* Response: *************"
 curl -d@msgs/SimpleWebService/sayHello http://localhost:8080/mock-jaxws/SimpleWebService
+echo ""
