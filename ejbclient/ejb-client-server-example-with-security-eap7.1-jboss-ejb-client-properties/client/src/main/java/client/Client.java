99ea155d349e039897c717ae39ff1a994388117b
Jan Martiska <jmartisk@redhat.com>
2017-03-07 12:17:43 +0100

    EAP 7.1 jboss ejb client properties example

---
+++ ejbclient/ejb-client-server-example-with-security-eap7.1-jboss-ejb-client-properties/client/src/main/java/client/Client.java
@@ -0,0 +1,35 @@
+package client;
+
+import java.security.PrivilegedActionException;
+import java.util.Properties;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import ejb.HelloBeanRemote;
+
+/**
+ * @author jmartisk
+ * @since 7/3/13
+ */
+public class Client {
+
+    public static void main(String[] args) throws NamingException, PrivilegedActionException {
+            try {
+                InitialContext ctx = new InitialContext(getCtxProperties());
+                String lookupName = "ejb:/server/HelloBean!ejb.HelloBeanRemote";
+                HelloBeanRemote bean = (HelloBeanRemote)ctx.lookup(lookupName);
+                System.out.println(bean.hello());
+                ctx.close();
+            } catch (NamingException e) {
+                throw new RuntimeException(e);
+            }
+    }
+
+    public static Properties getCtxProperties() {
+        Properties props = new Properties();
+        props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        return props;
+    }
+
+}
