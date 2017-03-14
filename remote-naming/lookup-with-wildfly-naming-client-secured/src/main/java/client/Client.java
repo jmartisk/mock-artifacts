90524aae47c43b1a2b52c098f628fc0a4f716085
Jan Martiska <jmartisk@redhat.com>
2017-03-14 12:38:22 +0100

    lookup with wildfly-naming-client from EAP 7.1

--- remote-naming/lookup-with-wildfly-naming-client-secured/src/main/java/client/Client.java
+++ remote-naming/lookup-with-wildfly-naming-client-secured/src/main/java/client/Client.java
@@ -5,7 +5,7 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import org.jboss.naming.remote.client.InitialContextFactory;
+import org.wildfly.naming.client.WildFlyInitialContextFactory;
 
 /**
  * @author jmartisk
@@ -26,12 +26,12 @@ public class Client {
 
     public static Properties getCtxProperties() {
         Properties props = new Properties();
-        props.put(Context.INITIAL_CONTEXT_FACTORY, InitialContextFactory.class.getName());
+        props.put(Context.INITIAL_CONTEXT_FACTORY, WildFlyInitialContextFactory.class.getName());
         props.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
         props.put(Context.SECURITY_PRINCIPAL, "joe");
         props.put(Context.SECURITY_CREDENTIALS, "joeIsAwesome2013!");
-	props.put("jboss.naming.client.connect.options.org.xnio.Options.SASL_DISALLOWED_MECHANISMS",
-	                "JBOSS-LOCAL-USER");
+        props.put("jboss.naming.client.connect.options.org.xnio.Options.SASL_DISALLOWED_MECHANISMS",
+                "JBOSS-LOCAL-USER");
         return props;
     }
 
