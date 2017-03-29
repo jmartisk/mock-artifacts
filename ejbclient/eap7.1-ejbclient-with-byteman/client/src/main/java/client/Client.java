789c0d5dff2f2b2e4a3484df615a4c3b205522f7
Jan Martiska <jmartisk@redhat.com>
2017-03-29 13:45:59 +0200

    ejb client + byteman example

---
+++ ejbclient/eap7.1-ejbclient-with-byteman/client/src/main/java/client/Client.java
@@ -0,0 +1,36 @@
+package client;
+
+import java.security.PrivilegedActionException;
+import java.util.Properties;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.wildfly.naming.client.WildFlyInitialContextFactory;
+
+import ejb.HelloBeanRemote;
+
+/**
+ * @author jmartisk
+ */
+public class Client {
+
+    public static void main(String[] args)
+            throws NamingException, PrivilegedActionException, InterruptedException {
+        InitialContext ctx = new InitialContext(getCtxProperties());
+        String lookupName = "ejb:/server/HelloBean!ejb.HelloBeanRemote";
+        HelloBeanRemote bean = (HelloBeanRemote)ctx.lookup(lookupName);
+        System.out.println(bean.hello());
+        ctx.close();
+    }
+
+    public static Properties getCtxProperties() {
+        Properties props = new Properties();
+        props.put(Context.INITIAL_CONTEXT_FACTORY, WildFlyInitialContextFactory.class.getName());
+        props.put(Context.PROVIDER_URL, "remote+http://127.0.0.1:8080");
+        props.put(Context.SECURITY_PRINCIPAL, "joe");
+        props.put(Context.SECURITY_CREDENTIALS, "joeIsAwesome2013!");
+        return props;
+    }
+
+}
