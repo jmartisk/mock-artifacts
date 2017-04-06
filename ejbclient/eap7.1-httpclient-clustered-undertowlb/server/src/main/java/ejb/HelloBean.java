b8185cc591685ac56a1f0ce42ad47b9de2bef44d
Jan Martiska <jmartisk@redhat.com>
2017-04-06 07:47:01 +0200

    update versions for EAP 7.1.0.DR16

--- ejbclient/eap7.1-httpclient-clustered-undertowlb/server/src/main/java/ejb/HelloBean.java
+++ ejbclient/eap7.1-httpclient-clustered-undertowlb/server/src/main/java/ejb/HelloBean.java
@@ -4,20 +4,27 @@ import javax.annotation.Resource;
 import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 
-import org.jboss.ejb3.annotation.SecurityDomain;
-
+//@Stateful
 @Stateless
 public class HelloBean implements HelloBeanRemote {
 
     @Resource
     SessionContext ctx;
 
+//    private Long counter;
+
     public HelloBean() {
     }
+
+//    @PostConstruct
+//    public void init() {
+//        counter = 0L;
+//    }
 
     @Override
     public String hello() {
         String nodeName = System.getProperty("jboss.node.name");
+//        System.out.println("method hello() invoked by user " + ctx.getCallerPrincipal().getName() + ", counter = " + ++counter);
         System.out.println("method hello() invoked by user " + ctx.getCallerPrincipal().getName());
         return ctx.getCallerPrincipal().getName() + " @ " + nodeName;
     }
