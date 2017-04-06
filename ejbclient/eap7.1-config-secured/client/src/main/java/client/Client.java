eae1175f4f2f69fab50d72914fa7f43e05079772
Jan Martiska <jmartisk@redhat.com>
2017-04-06 08:24:10 +0200

    set affinity.none

--- ejbclient/eap7.1-config-secured/client/src/main/java/client/Client.java
+++ ejbclient/eap7.1-config-secured/client/src/main/java/client/Client.java
@@ -3,10 +3,13 @@ package client;
 import java.security.PrivilegedActionException;
 import java.security.Security;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import org.jboss.ejb.client.Affinity;
+import org.jboss.ejb.client.EJBClient;
 import org.wildfly.naming.client.WildFlyInitialContextFactory;
 import org.wildfly.security.WildFlyElytronProvider;
 
@@ -19,9 +22,13 @@ public class Client {
         Security.addProvider(new WildFlyElytronProvider());       // FIXME this is a workaround and should not be needed
         InitialContext ctx = new InitialContext(getCtxProperties());
         try {
-            String lookupName = "ejb:/server/HelloBean!ejb.HelloBeanRemote";
+            String lookupName = "ejb:/server/HelloBean!ejb.HelloBeanRemote?stateful";
             HelloBeanRemote bean = (HelloBeanRemote)ctx.lookup(lookupName);
-            System.out.println(bean.hello());
+            EJBClient.setStrongAffinity(bean, Affinity.NONE);       // FIXME this is needed for DR16
+            for(int i = 0; i<20; i++) {
+                System.out.println(bean.hello());
+                TimeUnit.SECONDS.sleep(1);
+            }
         } finally {
             ctx.close();
         }
