5c35e4f1a03963c1df303f5b46ab2c4c5db954b9
Jan Martiska <jmartisk@redhat.com>
2017-04-27 15:40:53 +0200

    fix versions

--- tools/update_ejb_client_versions.sh
+++ tools/update_ejb_client_versions.sh
@@ -1,1 +1,1 @@
-for i in `find . -name pom.xml`; do echo $i; sed "s#7.1.0.Beta1-redhat-1#7.1.0.Beta2-redhat-1#g" -i $i; sed "s#7.1.0.DR16#7.1.0.DR17#g" -i $i; done
+for i in `find . -name pom.xml`; do echo $i; sed "s#7.1.0.Beta1-redhat-1#7.1.0.Beta1-redhat-2#g" -i $i; sed "s#7.1.0.DR16#7.1.0.DR17#g" -i $i; done
