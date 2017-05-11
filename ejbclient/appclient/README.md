ee6c226fbf605a19dace51d8467b4c204708f5f2
Jan Martiska <jmartisk@redhat.com>
2017-05-11 12:21:48 +0200

    fix ejb@appclient example

--- ejbclient/appclient/README.md
+++ ejbclient/appclient/README.md
@@ -1,8 +1,8 @@
 1. unzip EAP distro
 2. add user to EAP with this command:  `bin/add-user.sh -a -g users -u joe -p joeIsAwesome2013!`
-3. run EAP, bind its applicaiton interface to `127.0.0.8` (that's the default url specified in the `wildfly-config.xml`)
+3. run EAP
 4. build server side, deploy
 5. build client side jar
-6. run the appclient using `$EAP_HOME/bin/appclient.sh -Dwildfly.config.url=/path/to/wildfly-config.xml /path/to/client.jar`
-(`wildfly-config.xml` is located in the root directory)
+6. run the appclient using `$EAP_HOME/bin/appclient.sh /path/to/client.jar`
+   if you bound EAP to a different address than 127.0.0.1, add the appclient.sh argument --host=http-remoting://HOST:PORT (it needs to be before the path to client jar)
 
--- ejbclient/appclient/client/src/main/java/client/Main.java
+++ ejbclient/appclient/client/src/main/java/client/Main.java
@@ -6,6 +6,7 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import org.jboss.ejb.client.EJBClientContext;
 import org.wildfly.naming.client.WildFlyInitialContextFactory;
 
 import ejb.HelloBeanRemote;
@@ -14,6 +15,7 @@ public class Main {
 
     public static void main(String[] args)
             throws NamingException, PrivilegedActionException, InterruptedException {
+        EJBClientContext.getCurrent().getConfiguredConnections().forEach(connection -> System.out.println(connection.getDestination()));
         InitialContext ctx = new InitialContext(getCtxProperties());
         String lookupName = "ejb:/server/HelloBean!ejb.HelloBeanRemote";
         HelloBeanRemote bean = (HelloBeanRemote)ctx.lookup(lookupName);
