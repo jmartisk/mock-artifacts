85a093257fcb7b61975574db06983e82b8068328
Jan Martiska <jmartisk@redhat.com>
2017-06-21 12:34:05 +0200

    optimize imports

--- ejbclient/eap7.1-with-clustering-stateless/client/src/main/java/client/Client.java
+++ ejbclient/eap7.1-with-clustering-stateless/client/src/main/java/client/Client.java
@@ -1,30 +1,13 @@
 package client;
 
-import java.net.URI;
 import java.security.PrivilegedActionException;
-import java.security.Provider;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import org.jboss.ejb.client.Affinity;
-import org.jboss.ejb.client.ClusterAffinity;
-import org.jboss.ejb.client.EJBClient;
-import org.jboss.ejb.client.EJBClientCluster;
-import org.jboss.ejb.client.EJBClientConnection;
-import org.jboss.ejb.client.EJBClientContext;
-import org.jboss.ejb.client.EJBIdentifier;
-import org.jboss.ejb.client.StatefulEJBLocator;
-import org.jboss.ejb.client.StatelessEJBLocator;
-import org.jboss.ejb.client.URIAffinity;
-import org.jboss.ejb.protocol.remote.RemoteTransportProvider;
 import org.wildfly.naming.client.WildFlyInitialContextFactory;
-import org.wildfly.security.WildFlyElytronProvider;
-import org.wildfly.security.auth.client.AuthenticationConfiguration;
-import org.wildfly.security.auth.client.AuthenticationContext;
-import org.wildfly.security.auth.client.MatchRule;
 
 import ejb.HelloBeanRemote;
 
@@ -37,19 +20,9 @@ public class Client {
 
     public static void main(String[] args)
             throws NamingException, PrivilegedActionException, InterruptedException {
-    /*    final EJBClientContext ctx = new EJBClientContext.Builder()
-                .addTransportProvider(new RemoteTransportProvider())
-                .addClientConnection(
-                        new EJBClientConnection.Builder()
-                                .setDestination(URI.create(URL))
-                                .build()
-                ).build();
-        EJBClientContext.getContextManager().setGlobalDefault(ctx);*/
-
         final InitialContext ejbCtx = new InitialContext(getProps());
         final HelloBeanRemote bean = (HelloBeanRemote)ejbCtx
                 .lookup("ejb:/server/HelloBean!" + HelloBeanRemote.class.getName());
-
         while (true) {
             System.out.println(bean.hello());
             TimeUnit.SECONDS.sleep(1);
@@ -61,6 +34,5 @@ public class Client {
         props.put(Context.INITIAL_CONTEXT_FACTORY, WildFlyInitialContextFactory.class.getName());
         return props;
     }
-
 
 }
