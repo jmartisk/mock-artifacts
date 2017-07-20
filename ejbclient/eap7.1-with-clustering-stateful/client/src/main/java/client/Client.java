50d3dc5ab36244437356bc321fce11fd8b0e11ec
Jan Martiska <jmartisk@redhat.com>
2017-07-20 15:00:35 +0200

    remove some workarounds from stateful clustering example

--- ejbclient/eap7.1-with-clustering-stateful/client/src/main/java/client/Client.java
+++ ejbclient/eap7.1-with-clustering-stateful/client/src/main/java/client/Client.java
@@ -1,31 +1,12 @@
 package client;
 
-import java.net.URI;
-import java.security.PrivilegedActionException;
-import java.security.Provider;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
-import org.jboss.ejb.client.Affinity;
-import org.jboss.ejb.client.ClusterAffinity;
 import org.jboss.ejb.client.EJBClient;
-import org.jboss.ejb.client.EJBClientCluster;
-import org.jboss.ejb.client.EJBClientConnection;
-import org.jboss.ejb.client.EJBClientContext;
-import org.jboss.ejb.client.EJBIdentifier;
-import org.jboss.ejb.client.StatefulEJBLocator;
-import org.jboss.ejb.client.StatelessEJBLocator;
-import org.jboss.ejb.client.URIAffinity;
-import org.jboss.ejb.protocol.remote.RemoteTransportProvider;
 import org.wildfly.naming.client.WildFlyInitialContextFactory;
-import org.wildfly.security.WildFlyElytronProvider;
-import org.wildfly.security.auth.client.AuthenticationConfiguration;
-import org.wildfly.security.auth.client.AuthenticationContext;
-import org.wildfly.security.auth.client.MatchRule;
-import org.wildfly.security.sasl.SaslMechanismSelector;
 
 import ejb.HelloBeanRemote;
 
@@ -34,47 +15,19 @@ import ejb.HelloBeanRemote;
  */
 public class Client {
 
-    final static String URL = "remote+http://127.0.0.1:8080";
 
     public static void main(String[] args)
             throws Exception {
-       /* // FIXME try to get rid of programmatic context builder
-        final EJBClientContext ctx = new EJBClientContext.Builder()
-                .addTransportProvider(new RemoteTransportProvider())
-                .addClientCluster(
-                        new EJBClientCluster.Builder().setName("ejb").build()
-                )
-                .addClientConnection(
-                        new EJBClientConnection.Builder()
-                                .setDestination(URI.create(URL))
-                                .build()
-                ).build();
-        EJBClientContext.getContextManager().setGlobalDefault(ctx);
-*/
         final InitialContext ejbCtx = new InitialContext(getProps());
         final HelloBeanRemote bean = (HelloBeanRemote)ejbCtx
                 .lookup("ejb:/server/HelloBean!" + HelloBeanRemote.class.getName() + "?stateful");
-
-       /*
-        final StatefulEJBLocator<HelloBeanRemote> locator = EJBClient
-                .createSession(Affinity.NONE, HelloBeanRemote.class, "", "server", "HelloBean", "");
-        final HelloBeanRemote bean = EJBClient.createProxy(locator);
-        EJBClient.setStrongAffinity(bean, Affinity.NONE);*/
-
-
-//        EJBClient.setStrongAffinity(bean, Affinity.NONE);
-        EJBClient.setStrongAffinity(bean, new ClusterAffinity("ejb"));
-        EJBClient.setWeakAffinity(bean, Affinity.NONE);
         while (true) {
-            System.out.println(EJBClient.getStrongAffinity(bean));
-            System.out.println(EJBClient.getWeakAffinity(bean));
             try {
                 System.out.println(bean.hello());
             } catch(Exception e) {
                 System.out.println("Call failed!!!!");
                 e.printStackTrace();
             }
-
             TimeUnit.SECONDS.sleep(1);
         }
     }
@@ -82,6 +35,7 @@ public class Client {
     public static Properties getProps() {
         final Properties props = new Properties();
         props.put(Context.INITIAL_CONTEXT_FACTORY, WildFlyInitialContextFactory.class.getName());
+        props.put(EJBClient.CLUSTER_AFFINITY, "ejb"); //FIXME: needed until JBEAP-11124 is resolved
         return props;
     }
 
