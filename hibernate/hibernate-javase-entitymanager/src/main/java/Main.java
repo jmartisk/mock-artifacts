a24b15d8043ed3968db84206eea4ec958a11a005
Jan Martiska <jmartisk@redhat.com>
2017-09-01 08:37:15 +0200

    update and fix hibernate example

--- hibernate/hibernate-javase-entitymanager/src/main/java/Main.java
+++ hibernate/hibernate-javase-entitymanager/src/main/java/Main.java
@@ -1,23 +1,6 @@
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.concurrent.TimeUnit;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TemporalType;
-
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.AnnotationConfiguration;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.ejb.EntityManagerFactoryImpl;
-import org.hibernate.internal.SessionFactoryImpl;
-import org.hibernate.metamodel.source.internal.SessionFactoryBuilderImpl;
-import org.hibernate.type.StandardBasicTypes;
 
 /**
  * @author Jan Martiska
@@ -27,11 +10,15 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory pu = Persistence.createEntityManagerFactory("MainPU");
         EntityManager em = pu.createEntityManager();
-        em.getTransaction().begin();
-
-        StupidEntity stupidEntity = new StupidEntity();
-        em.persist(stupidEntity);
-        em.getTransaction().commit();
+        try {
+            em.getTransaction().begin();
+            StupidEntity stupidEntity = new StupidEntity();
+            em.persist(stupidEntity);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+            pu.close();
+        }
 
     }
 }
