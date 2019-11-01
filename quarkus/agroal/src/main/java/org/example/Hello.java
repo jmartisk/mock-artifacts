84724ccb98a350093b39764418f846c89586fd08
Jan Martiska <jmartisk@redhat.com>
2019-11-01 12:15:51 +0100

    update agroal example with metrics

--- quarkus/agroal/src/main/java/org/example/Hello.java
+++ quarkus/agroal/src/main/java/org/example/Hello.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.agroal.api.AgroalDataSource;
+import io.quarkus.agroal.DataSource;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -10,22 +11,40 @@ import javax.ws.rs.core.MediaType;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
 
 @Path("/")
 public class Hello {
 
     @Inject
-    AgroalDataSource agroalDataSource;
+    AgroalDataSource defaultDS;
+
+    @Inject
+    @DataSource(value = "ds1")
+    AgroalDataSource otherDS;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/")
     public String hello() throws SQLException {
-        try(Connection connection = agroalDataSource.getConnection()) {
+        try(Connection connection = defaultDS.getConnection()) {
             try(Statement statement = connection.createStatement()) {
                 statement.execute("SELECT 1");
             }
         }
-        return "ok";
+        return "ok, acquire count for datasource=default: "  + defaultDS.getMetrics().acquireCount();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/other")
+    public String other() throws SQLException, InterruptedException {
+        try(Connection connection = otherDS.getConnection()) {
+            try(Statement statement = connection.createStatement()) {
+                TimeUnit.SECONDS.sleep(5);
+                statement.execute("SELECT 1");
+            }
+        }
+        return "ok, acquire count for datasource=ds1: "  + otherDS.getMetrics().acquireCount();
     }
 }
