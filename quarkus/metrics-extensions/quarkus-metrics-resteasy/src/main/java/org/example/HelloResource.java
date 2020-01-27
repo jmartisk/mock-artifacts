0fa38011dabdd057d2e43e7afff8b9920f5d3b5b
Jan Martiska <jmartisk@redhat.com>
2020-01-27 13:33:33 +0100

    Array, List and Varargs REST methods for REST metrics example

--- quarkus/metrics-extensions/quarkus-metrics-resteasy/src/main/java/org/example/HelloResource.java
+++ quarkus/metrics-extensions/quarkus-metrics-resteasy/src/main/java/org/example/HelloResource.java
@@ -3,7 +3,9 @@ package org.example;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.PathSegment;
 import javax.ws.rs.core.Response;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ThreadLocalRandom;
@@ -31,6 +33,27 @@ public class HelloResource {
     public Long exception() throws InterruptedException {
         TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextInt(300));
         throw new RuntimeException("Yep!");
+    }
+
+    @GET
+    @Path("{segment}/{other}/{segment}/list")
+    public Response list(@PathParam("segment") List<PathSegment> segments) {
+        System.out.println("list segments: " + segments.size());
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("{segment}/{other}/{segment}/array")
+    public Response array(@PathParam("other") String other, @PathParam("segment") PathSegment[] segments) {
+        System.out.println("array segments: " + segments.length);
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("{segment}/{other}/{segment}/varargs")
+    public Response varargs(@PathParam("other") String other, @PathParam("segment") PathSegment... segments) {
+        System.out.println("array segments: " + segments.length);
+        return Response.ok().build();
     }
 
     @Path("/async")
