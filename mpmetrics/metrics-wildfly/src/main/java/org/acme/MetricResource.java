9221ff2e6bafe309373596cd5383360cbfde91be
Jan Martiska <jmartisk@redhat.com>
2020-04-02 15:23:23 +0200

    wildfly metrics basic example

---
+++ mpmetrics/metrics-wildfly/src/main/java/org/acme/MetricResource.java
@@ -0,0 +1,34 @@
+package org.acme;
+
+import org.eclipse.microprofile.metrics.annotation.ConcurrentGauge;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Metered;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.sse.Sse;
+import javax.ws.rs.sse.SseEventSink;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+@Path("/")
+public class MetricResource {
+
+    @GET
+    @Path("/counter")
+    @Counted(name = "annotated-counter23451", absolute = true)
+    @Produces("text/plain")
+    public String countedMethod2() {
+        return "ACK";
+    }
+
+
+}
