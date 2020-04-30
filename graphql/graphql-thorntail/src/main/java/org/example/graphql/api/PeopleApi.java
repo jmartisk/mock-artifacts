0f523a985fcf048d824be505912eab44edb16b06
Jan Martiska <jmartisk@redhat.com>
2020-04-30 08:29:31 +0200

    Add a @Source query

--- graphql/graphql-thorntail/src/main/java/org/example/graphql/api/PeopleApi.java
+++ graphql/graphql-thorntail/src/main/java/org/example/graphql/api/PeopleApi.java
@@ -1,10 +1,13 @@
 package org.example.graphql.api;
 
+import org.eclipse.microprofile.graphql.DefaultValue;
 import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.GraphQLException;
 import org.eclipse.microprofile.graphql.Mutation;
 import org.eclipse.microprofile.graphql.Name;
 import org.eclipse.microprofile.graphql.Query;
+import org.eclipse.microprofile.graphql.Source;
 import org.example.graphql.database.Database;
 import org.example.graphql.model.Gender;
 import org.example.graphql.model.Person;
@@ -12,6 +15,8 @@ import org.example.graphql.model.Person;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.util.Collection;
+import java.util.UUID;
+import java.util.logging.Level;
 
 @GraphQLApi
 @ApplicationScoped
@@ -20,12 +25,14 @@ public class PeopleApi {
     @Inject
     Database database;
 
+    // To try out, see queries/query-all-persons* files
     @Query(value = "all")
     @Description("Retrieve all persons from the database")
     public Collection<Person> all_methodName() {
         return database.allPersons();
     }
 
+    // To try out, see queries/mutation-create-person* files
     @Mutation(value = "create")
     @Description("Create a person")
     // TODO: allow passing a whole person including gender
@@ -33,6 +40,21 @@ public class PeopleApi {
         Person person = new Person(name, Gender.OTHER);
         database.addPerson(person);
         return person;
+    }
+
+    // This effectively adds a "secretToken" field to the Person type. It is random and different each time it is requested.
+    // To try out, see queries/generate-secret-tokens* files
+    @Name("secretToken")
+    public String generateSecretToken(@Source Person person,
+                                      @DefaultValue("true")
+                                      @Name("maskFirstPart") boolean maskFirstPart) {
+        String uuid = UUID.randomUUID().toString();
+        if (maskFirstPart) {
+            return uuid.substring(0, uuid.length() - 4).replaceAll("[A-Za-z0-9]", "*")
+                    + uuid.substring(uuid.length() - 4);
+        } else {
+            return uuid;
+        }
     }
 
 }
