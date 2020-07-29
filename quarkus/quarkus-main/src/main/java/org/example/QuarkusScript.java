cc55790a7db0f0d3c89066e3f331017f926dee6e
Jan Martiska <jmartisk@redhat.com>
2020-07-29 10:02:13 +0200

    add a main method

--- quarkus/quarkus-main/src/main/java/org/example/QuarkusScript.java
+++ quarkus/quarkus-main/src/main/java/org/example/QuarkusScript.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
 public class QuarkusScript implements QuarkusApplication {
+
+    // for running the script from inside an IDE
+    public static void main(String[] args) {
+        Quarkus.run(QuarkusScript.class, args);
+    }
 
     @Override
     public int run(String... args) throws Exception {
