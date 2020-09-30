050aeb1afd4ac0adb6fc38bd21ae76ccef75dbeb
Jan Martiska <jmartisk@redhat.com>
2020-09-30 11:27:50 +0200

    allow to configure the port

--- graphql/graphql-client/README.md
+++ graphql/graphql-client/README.md
@@ -1,4 +1,4 @@
 ## Standalone (pure Java SE) GraphQL client application
 
 - First, in another terminal, run the Quarkus project from `../graphql-quarkus`
-- Then, `mvn package exec:java`
+- Then, `mvn package exec:java [-Dport=8080]`
--- graphql/graphql-client/src/main/java/org/example/graphql/client/ClientMain.java
+++ graphql/graphql-client/src/main/java/org/example/graphql/client/ClientMain.java
@@ -5,8 +5,9 @@ import io.smallrye.graphql.client.typesafe.api.GraphQlClientBuilder;
 public class ClientMain {
 
     public static void main(String[] args) {
+        Integer port = Integer.getInteger("port", 8080);
         PeopleClientApi client = GraphQlClientBuilder.newBuilder()
-                .endpoint("http://localhost:8080/graphql")
+                .endpoint("http://localhost:" + port + "/graphql")
                 .build(PeopleClientApi.class);
         System.out.println(client.getAll());
     }
