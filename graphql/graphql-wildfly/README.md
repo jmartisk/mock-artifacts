624e07c0d7529410e62d7e6aba1a9285022a30c9
Jan Martiška <jmartisk@redhat.com>
2020-12-08 12:43:48 +0100

    improve!

--- graphql/graphql-wildfly/README.md
+++ graphql/graphql-wildfly/README.md
@@ -1,14 +1,19 @@
+## Provision a wildfly instance to use
+`galleon.sh provision provision.xml --dir=wildfly`
+
 ## Run
 - run a wildfly instance 
 - `mvn package wildfly:deploy`
 
 To enable various stuff, add this to wildfly:
 - `-Dsmallrye.graphql.validation.enabled=true`   
+- `-Dsmallrye.graphql.metrics.enabled=true` 
 
 ## Query
 Use GraphiQL interface at `http://localhost:8080/graphql-ui`
 Or use curl:
 - To get the schema, `curl localhost:8080/graphql/schema.graphql`
 - To run a query, `curl -H"Content-Type: application/json" -X POST localhost:8080/graphql -d @queries/query-all-persons.txt`
+- To run a query returning Uni, `curl -H"Content-Type: application/json" -X POST localhost:8080/graphql -d @queries/query-all-persons-uni.txt`
 - To run a mutation, `curl -H"Content-Type: application/json" -X POST localhost:8080/graphql -d @queries/mutation-create-person.txt`
 - To run a source query, `curl -H"Content-Type: application/json" -X POST localhost:8080/graphql -d @queries/generate-secret-tokens.txt`
--- graphql/graphql-wildfly/src/main/java/org/example/graphql/api/PeopleApi.java
+++ graphql/graphql-wildfly/src/main/java/org/example/graphql/api/PeopleApi.java
@@ -1,5 +1,7 @@
 package org.example.graphql.api;
 
+import io.smallrye.graphql.api.Context;
+import io.smallrye.mutiny.Uni;
 import org.eclipse.microprofile.graphql.DefaultValue;
 import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
@@ -12,17 +14,25 @@ import org.example.graphql.model.Person;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.CDI;
+import javax.inject.Inject;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @GraphQLApi
-@ApplicationScoped
+@RequestScoped
 public class PeopleApi {
 
     private List<Person> database = new ArrayList<>();
+
+    @Inject  // use Instance to be able to get a new instance multiple times - Context is @Dependent and therefore not proxied
+    private Instance<Context> context;
 
     @PostConstruct
     void init() {
@@ -33,8 +43,23 @@ public class PeopleApi {
     // To try out, see queries/query-all-persons* files
     @Query(value = "all")
     @Description("Retrieve all persons from the database")
-    public Collection<Person> all_methodName() {
+    public Collection<Person> all() {
+        if (!context.get().getFieldName().equals("all")) {
+            throw new IllegalStateException("Context seems to not have been set correctly");
+        }
         return database;
+    }
+
+    @Query(value = "allUni")
+    @Description("Retrieve all persons from the database")
+    public Uni<Collection<Person>> allUni() {
+        Supplier<Collection<Person>> supplier = () -> {
+            if (!context.get().getFieldName().equals("allUni")) {
+                throw new IllegalStateException("Context seems to not have been propagated correctly");
+            }
+            return database;
+        };
+        return Uni.createFrom().item(supplier);
     }
 
     // To try out, see queries/mutation-create-person* files
