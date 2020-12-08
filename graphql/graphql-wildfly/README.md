9da0b7ae8191e87cb7bce16cbfd0b69fde792371
Jan Martiška <jmartisk@redhat.com>
2020-12-08 10:42:34 +0100

    initial graphql-wildfly example

---
+++ graphql/graphql-wildfly/README.md
@@ -0,0 +1,14 @@
+## Run
+- run a wildfly instance 
+- `mvn package wildfly:deploy`
+
+To enable various stuff, add this to wildfly:
+- `-Dsmallrye.graphql.validation.enabled=true`   
+
+## Query
+Use GraphiQL interface at `http://localhost:8080/graphql-ui`
+Or use curl:
+- To get the schema, `curl localhost:8080/graphql/schema.graphql`
+- To run a query, `curl -H"Content-Type: application/json" -X POST localhost:8080/graphql -d @queries/query-all-persons.txt`
+- To run a mutation, `curl -H"Content-Type: application/json" -X POST localhost:8080/graphql -d @queries/mutation-create-person.txt`
+- To run a source query, `curl -H"Content-Type: application/json" -X POST localhost:8080/graphql -d @queries/generate-secret-tokens.txt`
---
+++ graphql/graphql-wildfly/src/main/java/org/example/graphql/api/PeopleApi.java
@@ -0,0 +1,63 @@
+package org.example.graphql.api;
+
+import org.eclipse.microprofile.graphql.DefaultValue;
+import org.eclipse.microprofile.graphql.Description;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Mutation;
+import org.eclipse.microprofile.graphql.Name;
+import org.eclipse.microprofile.graphql.Query;
+import org.eclipse.microprofile.graphql.Source;
+import org.example.graphql.model.Gender;
+import org.example.graphql.model.Person;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@GraphQLApi
+@ApplicationScoped
+public class PeopleApi {
+
+    private List<Person> database = new ArrayList<>();
+
+    @PostConstruct
+    void init() {
+        database.add(new Person("david", Gender.MALE));
+        database.add(new Person("jane", Gender.FEMALE));
+    }
+
+    // To try out, see queries/query-all-persons* files
+    @Query(value = "all")
+    @Description("Retrieve all persons from the database")
+    public Collection<Person> all_methodName() {
+        return database;
+    }
+
+    // To try out, see queries/mutation-create-person* files
+    @Mutation(value = "create")
+    @Description("Create a person")
+    public Person create_methodName(@Valid Person person) {
+        database.add(person);
+        return person;
+    }
+
+    // This effectively adds a "secretToken" field to the Person type. It is random and different each time it is requested.
+    // To try out, see queries/generate-secret-tokens* files
+    @Name("secretToken")
+    public String generateSecretToken(@Source Person person,
+                                      @DefaultValue("true")
+                                      @Name("maskFirstPart") boolean maskFirstPart) {
+        String uuid = UUID.randomUUID().toString();
+        if (maskFirstPart) {
+            return uuid.substring(0, uuid.length() - 4).replaceAll("[A-Za-z0-9]", "*")
+                    + uuid.substring(uuid.length() - 4);
+        } else {
+            return uuid;
+        }
+    }
+
+}
