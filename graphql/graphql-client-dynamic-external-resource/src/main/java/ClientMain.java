47e04c40121fdebb4221305c1d927a51e6be00f3
Jan Martiška <jmartisk@redhat.com>
2021-02-23 12:09:13 +0100

    ..

--- graphql/graphql-client-dynamic-external-resource/src/main/java/ClientMain.java
+++ graphql/graphql-client-dynamic-external-resource/src/main/java/ClientMain.java
@@ -1,6 +1,4 @@
-
-import io.smallrye.graphql.client.dynamic.RequestImpl;
-import io.smallrye.graphql.client.dynamic.SmallRyeGraphQLDynamicClient;
+import io.smallrye.graphql.client.dynamic.DynamicClientImpl;
 import io.vertx.core.Vertx;
 import org.eclipse.microprofile.graphql.client.core.Document;
 
@@ -15,17 +13,18 @@ public class ClientMain {
 
     // Retrieve a list of countries. See https://github.com/trevorblades/countries
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (SmallRyeGraphQLDynamicClient client = new SmallRyeGraphQLDynamicClient.Builder()
+        try (DynamicClientImpl client = new DynamicClientImpl.Builder()
                 .url("https://countries.trevorblades.com")
                 .build()) {
             Document document = document(
                     operation("CountriesQuery",
                             field("countries",
                                     field("name"))));
-            RequestImpl request = new RequestImpl(document.build());
-            JsonObject data = client.executeSync(request).getData();
+            JsonObject data = client.executeSync(document).getData();
             System.out.println(data.toString());
         }
-        Vertx.vertx().close();
+        finally {
+            Vertx.vertx().close();
+        }
     }
 }
