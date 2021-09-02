e123771e1d8f66b90226b16a0ae6880cf3bea43d
Jan Martiška <jmartisk@redhat.com>
2021-09-02 13:07:42 +0200

    update graphql-client-typesafe-standalone to latest smallrye version

--- graphql/graphql-client-typesafe-standalone/src/main/java/org/example/graphql/client/ClientMain.java
+++ graphql/graphql-client-typesafe-standalone/src/main/java/org/example/graphql/client/ClientMain.java
@@ -2,13 +2,19 @@ package org.example.graphql.client;
 
 import io.smallrye.graphql.client.typesafe.api.TypesafeGraphQLClientBuilder;
 
+import java.io.IOException;
+
 public class ClientMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Integer port = Integer.getInteger("port", 8080);
-        PeopleClientApi client = TypesafeGraphQLClientBuilder.newBuilder()
-                .endpoint("http://localhost:" + port + "/graphql")
-                .build(PeopleClientApi.class);
-        System.out.println(client.getAll());
+        // alternatively, set the url using a config property instead of passing it to the builder by calling endpoint()
+        // this works in smallrye-graphql 1.3.2+ with proper support for standalone (Java SE) apps
+//        System.setProperty("org.example.graphql.client.PeopleClientApi/mp-graphql/url", "http://localhost:" + port + "/graphql");
+        try (PeopleClientApi client = TypesafeGraphQLClientBuilder.newBuilder()
+            .endpoint("http://localhost:" + port + "/graphql")
+            .build(PeopleClientApi.class)) {
+            System.out.println(client.getAll());
+        }
     }
 }
--- graphql/graphql-client-typesafe-standalone/src/main/java/org/example/graphql/client/PeopleClientApi.java
+++ graphql/graphql-client-typesafe-standalone/src/main/java/org/example/graphql/client/PeopleClientApi.java
@@ -1,12 +1,14 @@
 package org.example.graphql.client;
 
+import io.smallrye.graphql.client.typesafe.api.GraphQLClientApi;
 import org.eclipse.microprofile.graphql.Query;
 import org.example.graphql.client.model.Person;
 
+import java.io.Closeable;
 import java.util.Collection;
 
-@io.smallrye.graphql.client.typesafe.api.GraphQLClientApi
-public interface PeopleClientApi {
+@GraphQLClientApi
+public interface PeopleClientApi  extends Closeable {
 
     @Query(value = "all")
     Collection<Person> getAll();
