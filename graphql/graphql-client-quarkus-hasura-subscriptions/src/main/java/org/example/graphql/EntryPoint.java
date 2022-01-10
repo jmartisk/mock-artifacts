8826c202b23c1b4b7b480fa903953d278a2f0415
Jan Martiška <jmartisk@redhat.com>
2022-01-10 11:39:57 +0100

    more progress

--- graphql/graphql-client-quarkus-hasura-subscriptions/src/main/java/org/example/graphql/EntryPoint.java
+++ graphql/graphql-client-quarkus-hasura-subscriptions/src/main/java/org/example/graphql/EntryPoint.java
@@ -1,5 +1,8 @@
 package org.example.graphql;
 
+import io.smallrye.graphql.client.Response;
+import io.smallrye.graphql.client.dynamic.api.DynamicGraphQLClient;
+import io.smallrye.graphql.client.dynamic.api.DynamicGraphQLClientBuilder;
 import io.smallrye.graphql.client.typesafe.api.TypesafeGraphQLClientBuilder;
 import io.smallrye.graphql.client.websocket.WebsocketSubprotocol;
 import io.smallrye.mutiny.Multi;
@@ -17,17 +20,18 @@ public class EntryPoint {
 
     private PersonApi typesafeClient;
     private Cancellable typesafeClientSubscription;
-    private Multi<List<Person>> personMulti;
+    private Multi<List<Person>> typesafeMulti;
 
     @Path("/typesafe/start")
     @GET
     public void startTypesafeClient() {
         typesafeClient = TypesafeGraphQLClientBuilder.newBuilder()
             .endpoint("http://localhost:7777/v1/graphql")
-            .subprotocols(WebsocketSubprotocol.GRAPHQL_TRANSPORT_WS)
+            .subprotocols(WebsocketSubprotocol.GRAPHQL_WS)
+//            .subprotocols(WebsocketSubprotocol.GRAPHQL_TRANSPORT_WS)
             .build(PersonApi.class);
-        personMulti = typesafeClient.person();
-        typesafeClientSubscription = personMulti.subscribe().with(list -> {
+        typesafeMulti = typesafeClient.person();
+        typesafeClientSubscription = typesafeMulti.subscribe().with(list -> {
             logger.info("Typesafe client received: " + list);
         }, failure -> {
             System.out.println("APPLICATION RECEIVED A FAILURE");
@@ -40,4 +44,32 @@ public class EntryPoint {
     public void stopTypesafeClient() {
         typesafeClientSubscription.cancel();
     }
+
+    private DynamicGraphQLClient dynamicClient;
+    private Cancellable dynamicClientSubscription;
+    private Multi<Response> dynamicMulti;
+
+    @Path("/dynamic/start")
+    @GET
+    public void startDynamicClient() {
+        dynamicClient = DynamicGraphQLClientBuilder.newBuilder()
+            .url("http://localhost:7777/v1/graphql")
+            .subprotocols(WebsocketSubprotocol.GRAPHQL_WS)
+//            .subprotocols(WebsocketSubprotocol.GRAPHQL_TRANSPORT_WS)
+            .build();
+        dynamicMulti = dynamicClient.subscription("subscription person { person {id name age} }");
+        dynamicClientSubscription = dynamicMulti.subscribe().with(list -> {
+            logger.info("Dynamic client received: " + list);
+        }, failure -> {
+            System.out.println("APPLICATION (DYNAMIC CLIENT) RECEIVED A FAILURE");
+            failure.printStackTrace();
+        });
+    }
+
+    @Path("/dynamic/stop")
+    @GET
+    public void stopDynamicClient() {
+        dynamicClientSubscription.cancel();
+    }
+
 }
