0e1f217ba6bc8102b9041b44fecec367b9c95208
Jan Martiška <wraychus@gmail.com>
2022-05-17 09:10:42 +0200

    update

--- graphql/graphql-client-dynamic-standalone-external-resource/src/main/java/ClientMain.java
+++ graphql/graphql-client-dynamic-standalone-external-resource/src/main/java/ClientMain.java
@@ -1,6 +1,6 @@
 import io.smallrye.graphql.client.core.Document;
 import io.smallrye.graphql.client.dynamic.api.DynamicGraphQLClient;
-import io.smallrye.graphql.client.dynamic.api.DynamicGraphQLClientBuilder;
+import io.smallrye.graphql.client.vertx.dynamic.VertxDynamicGraphQLClientBuilder;
 import io.vertx.core.Vertx;
 
 import javax.json.JsonObject;
@@ -13,21 +13,20 @@ import static io.smallrye.graphql.client.core.Operation.operation;
 public class ClientMain {
 
     // Retrieve a list of countries. See https://github.com/trevorblades/countries
-    public static void main(String[] args) {
-        try (DynamicGraphQLClient client = DynamicGraphQLClientBuilder.newBuilder()
-              .url("https://countries.trevorblades.com")
-              .build()) {
+    public static void main(String[] args) throws Exception {
+        Vertx vertx = Vertx.vertx();
+        try (DynamicGraphQLClient client = new VertxDynamicGraphQLClientBuilder()
+            .url("https://countries.trevorblades.com")
+            .vertx(vertx)
+            .build()) {
             Document document = document(
                 operation(field("countries",
                     field("name"))));
             JsonObject data = client.executeSync(document).getData();
             System.out.println(data.toString());
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
         finally {
-            Vertx.vertx().close();
+            vertx.close();
         }
     }
 }
