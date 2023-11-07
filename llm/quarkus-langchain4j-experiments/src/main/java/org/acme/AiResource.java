22f35b277005456f4916bfaa5473b3a5db48a070
Jan Martiska <jmartisk@redhat.com>
2023-11-07 08:24:36 +0100

    Embeddings

---
+++ llm/quarkus-langchain4j-experiments/src/main/java/org/acme/AiResource.java
@@ -0,0 +1,66 @@
+package org.acme;
+
+import dev.langchain4j.data.document.Metadata;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.AllMiniLmL6V2QuantizedEmbeddingModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.output.Response;
+import dev.langchain4j.store.embedding.EmbeddingMatch;
+import io.quarkiverse.langchain4j.redis.RedisEmbeddingStore;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/")
+public class AiResource {
+
+    @Inject
+    RedisEmbeddingStore store;
+
+    private final EmbeddingModel embeddingModel = new AllMiniLmL6V2QuantizedEmbeddingModel();
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/embeddings")
+    public void embeddings() {
+        embed("Charlie is wearing a hat", Metadata.from("k1", "v1"));
+        embed("Charlie has a big tummy");
+        embed("Charlie broke his bulldozer");
+        embed("Charlie has a lot of toys");
+        embed("Charlie likes construction vehicles");
+        embed("One year ago, Charlie was three years old");
+        embed("David can move stuff around with his mind");
+        embed("Pedro can count");
+        embed("Charlie likes candy");
+        embed("I believe in aliens");
+
+        Response<Embedding> question = embeddingModel.embed("Does Charlie have a bulldozer?");
+        List<EmbeddingMatch<TextSegment>> relevantEmbeddings = store.findRelevant(question.content(), 5);
+
+        for (EmbeddingMatch<TextSegment> match : relevantEmbeddings) {
+            String text = match.embedded() != null ? match.embedded().text() : null;
+            Metadata metadata = match.embedded() != null ? match.embedded().metadata() : null;
+            System.out.println();
+            System.out.println("Relevant embedding ID = " + match.embeddingId() + "\n" +
+                    "text = " + text + "\n" +
+                    "metadata = " + metadata + "\n" +
+                    "score = " + match.score());
+        }
+    }
+
+    private void embed(String string) {
+        embed(string, new Metadata());
+    }
+
+    private void embed(String string, Metadata metadata) {
+        TextSegment textSegment = new TextSegment(string, metadata);
+        Embedding embedding = embeddingModel.embed(textSegment).content();
+        store.add(embedding, textSegment);
+    }
+
+}
